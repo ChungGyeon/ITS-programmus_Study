b9ab5d41b1f7c77050ccc52caac67f4709a32208
임재혁 <imsdm@naver.com>
2025-04-06 21:08:24 +0900

    week1 :옹알이
    겹치는 단어가 있으면 그걸 제거하는 식으로 접근
    다만 단어가 2개이상 겹칠 시 처리할 방법에 조금 시간이 걸림

--- print("test").py
+++ print("test").py
@@ -1,1 +1,15 @@
-print("test")
+def solution(babbling):
+    babbling_dict = ["aya", "ye", "woo", "ma"]
+    answer = 0
+    for word in babbling:
+        for babble in babbling_dict:
+            if babble in word:
+                word = word.replace(babble, " ")
+        if len(word.strip()) == 0:
+            answer += 1
+
+    return answer
+
+babbling_dict = ["ayaye", "uuuma", "ye", "yemawoo", "ayaa"]
+print(solution(babbling_dict))
+
