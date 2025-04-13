4f969d7f84bfed0c9fc5f1a8232b45004aedf90f
임재혁 <imsdm@naver.com>
2025-04-14 08:12:44 +0900

    week2 종이 자르기
    2x2로 된 종이를 1x1로 만드는건 3번의 가위질
    2x5로 된 종이는 9번의 가위질
    m*n - 1
    한번넣어보니 오잉 된다

--- print("test").py
+++ print("test").py
@@ -1,8 +1,7 @@
-def solution(num, total):
-    start = (total // num) - (num - 1) // 2
-    answer = [start+i for i in range(num)]
+def solution(M, N):
+    answer = (M*N)-1
     return answer
 
-print(solution(3, 12))  
-print(solution(5, 15))  
-print(solution(4, 14)) 
+print(solution(2, 2))  # 출력: 3
+print(solution(3, 3))  # 출력: 8
+print(solution(1, 5))  # 출력: 4
