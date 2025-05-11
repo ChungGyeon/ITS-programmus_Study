a0bebedb0b4322b62559d6e2ed71ede37ce672f5
임재혁 <imsdm@naver.com>
2025-05-11 18:18:00 +0900

    week4 구명보트

--- print("test").py
+++ print("test").py
@@ -1,18 +1,42 @@
-def solution(num):
-    answer=0
-    if(num==1):
-         answer=0
-    while(num!=1):
-         if(answer>=500):
-              return -1
-         if(num%2==0):
-              num/=2
-         else:
-              num=(num*3)+1
-         answer+=1     
-    return answer
+# 앞놈을 잡고, 뒤까지 쭉 조사한 뒤 조건에 맞으면 같이 보내고 아니면 안보내는 형태로
+def solution(people, limit):
+    cnt=0
+    sml=0
+    big=len(people)-1
+    people.sort()
+    while(sml<=big):
+        if(people[sml]+people[big]<=limit):
+            sml+=1
+        big-=1
+        cnt+=1
+    return cnt
+        
+    
+            
+people = [100, 200, 150, 80]  
+limit=200
+print(solution(people,limit))
 
-print(solution(626331))
+
+"""
+def solution(people, limit):
+    cnt=0
+    sum=people.pop(0)
+    for i in people:
+        if(sum==limit):
+            cnt+=1
+            sum=0
+        if((sum+i)<=limit):
+            sum+=i
+            people.remove(i)
+    if(people):
+        cnt+=1
+        return solution(people,limit)+cnt
+    else:
+        return cnt+1
+"""
+
+            
 
 
 
