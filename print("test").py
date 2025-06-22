de98ddc47f771dd87280e7f31599c943aee56f78
임재혁 <imsdm@naver.com>
2025-06-22 21:09:47 +0900

    week 5 큰 수 만들기

--- print("test").py
+++ print("test").py
@@ -1,40 +1,25 @@
-# 앞놈을 잡고, 뒤까지 쭉 조사한 뒤 조건에 맞으면 같이 보내고 아니면 안보내는 형태로
-def solution(people, limit):
-    cnt=0
-    sml=0
-    big=len(people)-1
-    people.sort()
-    while(sml<=big):
-        if(people[sml]+people[big]<=limit):
-            sml+=1
-        big-=1
-        cnt+=1
-    return cnt
-        
-    
+"""어떤 숫자에서 k개의 수를 제거했을 때 얻을 수 있는 가장 큰 숫자를 구하려 합니다.
+
+예를 들어, 숫자 1924에서 수 두 개를 제거하면 [19, 12, 14, 92, 94, 24] 를 만들 수 있습니다. 
+이 중 가장 큰 숫자는 94 입니다.
+
+문자열 형식으로 숫자 number와 제거할 수의 개수 k가 solution 함수의 매개변수로 주어집니다.
+number에서 k 개의 수를 제거했을 때 만들 수 있는 수 중 가장 큰 숫자를 문자열 형태로
+return 하도록 solution 함수를 완성하세요.
+"""
+def solution(number, k):
+    number = str(number)
+    stack = []
+    for num in number:
+        while k > 0 and stack and stack[-1] < num:
+            stack.pop()
+            k -= 1
+        stack.append(num)
+    answer = int(stack[0]+stack[1])
+    return answer
+
             
-people = [100, 200, 150, 80]  
-limit=200
-print(solution(people,limit))
-
-
-"""
-def solution(people, limit):
-    cnt=0
-    sum=people.pop(0)
-    for i in people:
-        if(sum==limit):
-            cnt+=1
-            sum=0
-        if((sum+i)<=limit):
-            sum+=i
-            people.remove(i)
-    if(people):
-        cnt+=1
-        return solution(people,limit)+cnt
-    else:
-        return cnt+1
-"""
+print(solution(1924,2))
 
             
 
