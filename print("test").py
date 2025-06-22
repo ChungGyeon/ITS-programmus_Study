1eea1edef26a848cb13db2210595e844f561984e
임재혁 <imsdm@naver.com>
2025-06-22 22:03:47 +0900

    week 5 체육복

--- print("test").py
+++ print("test").py
@@ -1,25 +1,35 @@
-"""어떤 숫자에서 k개의 수를 제거했을 때 얻을 수 있는 가장 큰 숫자를 구하려 합니다.
-
-예를 들어, 숫자 1924에서 수 두 개를 제거하면 [19, 12, 14, 92, 94, 24] 를 만들 수 있습니다. 
-이 중 가장 큰 숫자는 94 입니다.
-
-문자열 형식으로 숫자 number와 제거할 수의 개수 k가 solution 함수의 매개변수로 주어집니다.
-number에서 k 개의 수를 제거했을 때 만들 수 있는 수 중 가장 큰 숫자를 문자열 형태로
-return 하도록 solution 함수를 완성하세요.
 """
-def solution(number, k):
-    number = str(number)
-    stack = []
-    for num in number:
-        while k > 0 and stack and stack[-1] < num:
-            stack.pop()
-            k -= 1
-        stack.append(num)
-    answer = int(stack[0]+stack[1])
+점심시간에 도둑이 들어, 일부 학생이 체육복을 도난당했습니다.
+ 다행히 여벌 체육복이 있는 학생이 이들에게 체육복을 빌려주려 합니다.
+학생들의 번호는 체격 순으로 매겨져 있어, 
+바로 앞번호의 학생이나 바로 뒷번호의 학생에게만 체육복을 빌려줄 수 있습니다. 
+예를 들어, 4번 학생은 3번 학생이나 5번 학생에게만 체육복을 빌려줄 수 있습니다. 
+체육복이 없으면 수업을 들을 수 없기 때문에 체육복을 적절히 빌려 최대한 많은 학생이 체육수업을 들어야 합니다.
+
+전체 학생의 수 n, 체육복을 도난당한 학생들의 번호가 담긴 배열 lost, 
+여벌의 체육복을 가져온 학생들의 번호가 담긴 배열 reserve가 매개변수로 주어질 때, 
+체육수업을 들을 수 있는 학생의 최댓값을 return 하도록 solution 함수를 작성해주세요.
+"""
+def solution(n, lost, reserve):
+    lost_set=set(lost)
+    reserve_set=set(reserve)
+    lapover=lost_set & reserve_set
+    lost_set-=lapover
+    reserve_set-=lapover
+    reserve=list(reserve_set)
+    lost=list(lost_set)
+    for i in reserve:
+        if i-1 in lost:
+            lost.remove(i-1)
+        elif i+1 in lost:
+            lost.remove(i+1)
+
+    answer = (n-len(lost))
     return answer
 
-            
-print(solution(1924,2))
+lost = [3]
+reserve = [1]
+print(solution(3,lost,reserve))
 
             
 
